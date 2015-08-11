4de01374d0c1cb63d1e92e95c03ad285ebd39ee1
dong yoon kang <yoon@dongs-MacBook-Pro.local>
2015-08-10 20:44:31 -0700

    Initial Commit

---
+++ Anchor ManTests/Anchor_ManTests.swift
@@ -0,0 +1,36 @@
+//
+//  Anchor_ManTests.swift
+//  Anchor ManTests
+//
+//  Created by dong yoon kang on 8/10/15.
+//  Copyright (c) 2015 yoon kevin kang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Anchor_ManTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
