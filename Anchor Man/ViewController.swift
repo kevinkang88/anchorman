8d762e936a8b9b717235d322d2710fdf1092aa06
dong yoon kang <yoon@dongs-MacBook-Pro.local>
2015-08-11 22:19:19 -0700

    first commit

--- Anchor Man/ViewController.swift
+++ Anchor Man/ViewController.swift
@@ -10,16 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var cityTextField: UITextField!
+
+    @IBOutlet var statusLabel: UILabel!
+    
+    var currentWeatherForecast = ""
+    
+    func showError(){
+        statusLabel.text = "Cannot locate your city"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func findTapped(sender: AnyObject) {
+        NSLog("find button tapped, begin search")
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        if(url != nil){
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                var urlError = false
+                
+                var weather = ""
+                
+                if error == nil {
+                    var urlContent = NSString(data: data , encoding: NSUTF8StringEncoding)
+                    println(urlContent)
+                    var urlContentArray = urlContent?.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlContentArray!.count > 0 {
+                        var weatherArray = urlContentArray![1].componentsSeparatedByString("</span>")
+                        weather = weatherArray[0] as! String
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                    }else {
+                        urlError = true
+                    }
+                    
+                } else{
+                    urlError = true
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    if urlError == true {
+                        self.showError()
+                    }else{
+                        self.statusLabel.text = weather
+                        self.currentWeatherForecast = weather
+                        // present results VC
+                    }
+                })
+                
+                
+                
+            })
+            
+            task.resume()
+        }else{
+            self.showError()
+        }
 
-
+    }
 }
 
